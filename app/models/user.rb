c987c570b9932762363630e3f03ea88524728d87
Nataly Blum <nblyumenshteyn@gmail.com>
2021-04-23 23:54:19 +0400

    fix validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
   validates :first_name, :last_name, :email, presence: true
   validates :first_name, :last_name, length: { minimum: 2 }
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: '%{value} invalid email' }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :email, uniqueness: true
 end
--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :name, :description] do |n|
     "string#{n}"
   end
-  sequence :string_email, aliases: [:email] do |n|
+  sequence :email do |n|
     "ff#{n}@ff.com"
   end
 end
